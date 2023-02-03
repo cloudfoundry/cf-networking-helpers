413c07799f0e4a194db8a436a4b2cedfe5a7a115
Josh Russett <jrussett@vmware.com>
2023-02-03 22:40:53 +0000

    Remove healthchecker (again)
    This reverts commit a2d7c75783c32052b7414cb1070bccc615cc79f4.

--- healthchecker/watchdog/watchdog_linux_test.go
+++
@@ -1,5 +0,0 @@
-package watchdog_test
-
-import "syscall"
-
-var TEST_SIGNAL = syscall.SIGUSR1
--- healthchecker/watchdog/watchdog_windows_test.go
+++
@@ -1,5 +0,0 @@
-package watchdog_test
-
-import "golang.org/x/sys/windows"
-
-var TEST_SIGNAL = windows.SIGTERM
