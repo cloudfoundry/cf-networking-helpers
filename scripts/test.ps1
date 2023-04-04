f8a80b96d807b9561f01b3d4036fc91c6984b380
David Sabeti <sabetid@vmware.com>
2023-04-04 22:35:04 +0000

    Skip db packages on windows

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -8,7 +8,8 @@ Write-Host "Running unit tests for packages targeted to build on Windows..."
 $env:SQL_FLAVOR="mysql"
 
 
-go run github.com/onsi/ginkgo/v2/ginkgo -r -keep-going -trace -randomize-all -race -r
+go run github.com/onsi/ginkgo/v2/ginkgo -r -keep-going -trace -randomize-all -race -r `
+  --skip-package=db,timeouts
 
 if ($LastExitCode -ne 0) {
   Write-Host "cf-networking-helpers unit tests failed"
